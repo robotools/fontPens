13e74cf56c10985574783064f282e88f13498e35
Jens Kutilek <webmail@kutilek.de>
2018-05-02 16:45:02 +0200

    Add identifiers and kwargs in ThresholdPointPen

--- Lib/fontPens/thresholdPointPen.py
+++ Lib/fontPens/thresholdPointPen.py
@@ -21,51 +21,51 @@ class ThresholdPointPen(AbstractPointPen):
         self._offCurveBuffer = []
         self.otherPointPen = otherPointPen
 
-    def beginPath(self, identifier=None):
+    def beginPath(self, identifier=None, **kwargs):
         """Start a new sub path."""
-        self.otherPointPen.beginPath(identifier)
+        self.otherPointPen.beginPath(identifier) # how to add kwargs?
         self._lastPt = None
 
     def endPath(self):
         """End the current sub path."""
         self.otherPointPen.endPath()
 
-    def addPoint(self, pt, segmentType=None, smooth=False, name=None, **kwargs):
+    def addPoint(self, pt, segmentType=None, smooth=False, name=None, identifier=None, **kwargs):
         """Add a point to the current sub path."""
         if segmentType in ['curve', 'qcurve']:
             # it's an offcurve, let's buffer them until we get another oncurve
             # and we know what to do with them
-            self._offCurveBuffer.append((pt, segmentType, smooth, name, kwargs))
+            self._offCurveBuffer.append((pt, segmentType, smooth, name, identifier, kwargs))
             return
 
         elif segmentType == "move":
             # start of an open contour
-            self.otherPointPen.addPoint(pt, segmentType, smooth, name)  # how to add kwargs?
+            self.otherPointPen.addPoint(pt, segmentType, smooth, name, identifier) # how to add kwargs?
             self._lastPt = pt
             self._offCurveBuffer = []
 
         elif segmentType == "line":
             if self._lastPt is None:
-                self.otherPointPen.addPoint(pt, segmentType, smooth, name)  # how to add kwargs?
+                self.otherPointPen.addPoint(pt, segmentType, smooth, name, identifier) # how to add kwargs?
                 self._lastPt = pt
             elif distance(pt, self._lastPt) >= self.threshold:
                 # we're oncurve and far enough from the last oncurve
                 if self._offCurveBuffer:
                     # empty any buffered offcurves
-                    for buf_pt, buf_segmentType, buf_smooth, buf_name, buf_kwargs in self._offCurveBuffer:
-                        self.otherPointPen.addPoint(buf_pt, buf_segmentType, buf_smooth, buf_name)  # how to add kwargs?
+                    for buf_pt, buf_segmentType, buf_smooth, buf_name, buf_identifier, buf_kwargs in self._offCurveBuffer:
+                        self.otherPointPen.addPoint(buf_pt, buf_segmentType, buf_smooth, buf_name, buf_identifier) # how to add kwargs?
                     self._offCurveBuffer = []
                 # finally add the oncurve.
-                self.otherPointPen.addPoint(pt, segmentType, smooth, name)  # how to add kwargs?
+                self.otherPointPen.addPoint(pt, segmentType, smooth, name, identifier) # how to add kwargs?
                 self._lastPt = pt
             else:
                 # we're too short, so we're not going to make it.
                 # we need to clear out the offcurve buffer.
                 self._offCurveBuffer = []
 
-    def addComponent(self, baseGlyphName, transformation, identifier=None):
+    def addComponent(self, baseGlyphName, transformation, identifier=None, **kwargs):
         """Add a sub glyph. Note: this way components are not filtered."""
-        self.otherPointPen.addComponent(baseGlyphName, transformation, identifier)
+        self.otherPointPen.addComponent(baseGlyphName, transformation, identifier) # how to add kwargs?
 
 
 # =========
